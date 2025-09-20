e08d69f57ce09f82184b1c955da48dd6957595c6
stoyj <stoyanka.kamenova@gmail.com>
2025-09-20 17:58:16 +0300

    // import Hello from "./Hello"; // import Counter from "./Counter"; // import TextInput from "./TextInput"; // import GreetingForm from "./GreetingForm";

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import Hello from "./Hello";
-import Counter from "./Counter";
-import TextInput from "./TextInput";
-import GreetingForm from "./GreetingForm";
+// import Hello from "./Hello";
+// import Counter from "./Counter";
+// import TextInput from "./TextInput";
+// import GreetingForm from "./GreetingForm";
+import GreetingForm from "./components/GreetingForm/GreetingForm.jsx";
 
 function App() {
   return (
     <div>
-      <Hello name="Stoyanka" />
+      {/* <Hello name="Stoyanka" />
       <Hello name="Minka" />
       <Hello name="React Hero" />
       <Hello name="React Learner" />
       <Counter />
       <TextInput />
+      <GreetingForm /> */}
       <GreetingForm />
     </div>
   );
